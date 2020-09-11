ygyglg/hdl

adrv9001/zcu102: Add debug header

projects/adrv9001/zcu102/system_top.v
@@ -122,7 +122,11 @@ module system_top (
 
   inout                   sm_fan_tach,
   output                  vadj_test_1,
-  output                  vadj_test_2
+  output                  vadj_test_2,
+
+  //debug hdr
+  output       [9:0]      proto_hdr
+
 );
   // internal registers
   reg         [  2:0] mcs_sync_m = 'd0;
@@ -258,8 +262,18 @@ module system_top (
     .spi1_sclk (),
     .spi1_csn (),
     .spi1_miso (1'b0),
-    .spi1_mosi ()
+    .spi1_mosi (),
+
+    // debug
+    .adc1_div_clk (proto_hdr[0]),
+    .adc2_div_clk (proto_hdr[1]),
+    .dac1_div_clk (proto_hdr[2]),
+    .dac2_div_clk (proto_hdr[3])
+
   );
+
+  assign proto_hdr[9:4] = {'b0};
+
 endmodule
 
 // ***************************************************************************

mfkiwl/hdl-JESD204

projects/adrv9001/zcu102/system_top.v
@@ -122,7 +122,11 @@ module system_top (
 
   inout                   sm_fan_tach,
   input                   vadj_err,
-  output                  platform_status
+  output                  platform_status,
+
+  //debug hdr
+  output       [9:0]      proto_hdr
+
 );
   // internal registers
   reg         [  2:0] mcs_sync_m = 'd0;
@@ -259,8 +263,18 @@ module system_top (
     .spi1_sclk (),
     .spi1_csn (),
     .spi1_miso (1'b0),
-    .spi1_mosi ()
+    .spi1_mosi (),
+
+    // debug
+    .adc1_div_clk (proto_hdr[0]),
+    .adc2_div_clk (proto_hdr[1]),
+    .dac1_div_clk (proto_hdr[2]),
+    .dac2_div_clk (proto_hdr[3])
+
   );
+
+  assign proto_hdr[9:4] = {'b0};
+
 endmodule
 
 // ***************************************************************************